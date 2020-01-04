7576984f498b432e78a670d5d68f3ccf4394ad76
Tim de Ganon <tim@deganon.com>
2020-01-04 10:30:30 -0500

    base schema

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, session
+import random
+imort string
 from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 import json
 
 config = json.loads(open("config.json").read())
 app.secret_key = config["secret"]
+
+def random_string():
+    return ''.join([random.choice(string.ascii_letters + string.ascii_uppercase + string.digits) for i in range(16)])
 
 @app.route('/api/signup', methods=['POST'])
 def signup():
@@ -24,3 +29,15 @@ def login():
         return ("success", 200)
     else:
         return ("wrong password", 403)
+
+@app.route('/api/create_meetup', methods=['POST'])
+def create_meetup():
+    meetup_info = {
+        "id": random_string(),
+        "name": request.form["name"],
+        "address": request.form["address"], # TODO: generate/validate with openstreetmap
+        "description": request.form["description"],
+        "key": request.form["key"], # TODO: validate with gnupg
+        "participants": []
+    }
+    
