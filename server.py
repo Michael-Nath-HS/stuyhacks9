3464956829610c8d98642418e312f17d875566b5
Michael <mnath10@stuy.edu>
2020-01-04 12:31:36 -0500

    created a user and event table that will store every user and the events that are created.

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, session, g
+import os
 from flask_sqlalchemy import SQLAlchemy
-import sqlite3
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 import json
@@ -28,35 +31,52 @@ def login():
         return ("wrong password", 403)
 
 
-database = "pathtodatabase"
-# con = sqlite3.connect("./db.sqlite3")
-def db_connect(db_path=database):
-    con = sqlite3.connect(db_path)
-    return con
-
-con = db_connect()
+db = SQLAlchemy(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    ownership = relationship("Event", backref="owner")
 
-#
-# def get_db():
-#     db = getattr(g, "_database", None)
-#     if db is None:
-#         db = g._database = sqlite3.connect(database)
-#     return db
-#
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
-#
-# @app.route("/")
-# def index():
-#     cur = get_db().cursor()
-#
-# def make_dicts(cursor, row):
-#     return dict((cursor.description[idx][0], value)
-#                 for idx, value in enumerate(row))
-#
-# db.row_factory = make_dicts
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Event(db.Model):
+    __tablename__ = "events"
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.String(16), unique=True, nullable=True)
+    host = db.Column(db.String, ForeignKey("users.username"))
+    event_address = db.Column(db.String(128), nullable=True)
+    event_description = db.Column(db.String(250), nullable=True)
+    event_name = db.Column(db.String(50), nullable=True)
+
+    def __init__(self, id, event_id, host, event_address, event_description, event_name):
+        self.id = id
+        self.event_id = event_id
+        self.host = host
+        self.event_address = event_address
+        self.event_description = event_description
+        self.event_name = event_name
+
+    def __repr__(self):
+        return '<Event %r>' % self.event_name
+    # participants = db.column(db.String(100), nullable=True)
+
